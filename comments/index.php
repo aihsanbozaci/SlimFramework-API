61e2dab5ca7c1eea7436d0532ab9d602389318db
Ahmet İhsan Bozacı <ahmetbozac@gmail.com>
2024-03-25 02:13:06 +0300

    Adding comments and posts codes and  show spesific post comments

--- comments/index.php
+++ comments/index.php
@@ -1,4 +1,5 @@
 <?php
+
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
@@ -24,7 +25,6 @@ $app->get('/slim4/comments/', function (Request $request, Response $response, $a
                 ->withHeader("Content-Type", 'application/json')
                 ->withStatus(200);
         }
-
     } catch (PDOException $e) {
         $response->getBody()->write(json_encode(
             array(
@@ -36,9 +36,104 @@ $app->get('/slim4/comments/', function (Request $request, Response $response, $a
             JSON_PRETTY_PRINT
         ));
         return $response->withHeader("Content-Type", 'application/json')->withStatus(500);
+    } finally {
+        $db = null;
     }
 
     return $response;
 });
+
+// adding comments from given example link
+$app->post('/slim4/comments/add', function (Request $request, Response $response, $args) {
+    $db = new Db();
+    try {
+        $db = $db->connect();
+
+        $data = $request->getBody()->getContents();
+        $comments = json_decode($data, true);
+
+        if (!$comments) {
+            $response->getBody()->write(json_encode(
+                array(
+                    "error" => array(
+                        'text' => "Eksik veya hatalı veri"
+                    )
+                ),
+                JSON_PRETTY_PRINT
+            ));
+            return $response->withHeader("Content-Type", 'application/json')->withStatus(400);
+        }
+
+        foreach ($comments as $comment) {
+            $postId = $comment["postId"] ?? null;
+            $id = $comment['id'] ?? null;
+            $name = $comment["name"] ?? null;
+            $body = $comment["body"] ?? null;
+            $email = $comment["email"] ?? null;
+
+            if (!$postId || !$name || !$body || !$email || !$id) {
+                $response->getBody()->write(json_encode(
+                    array(
+                        "error" => array(
+                            'text' => "Eksik parametre"
+                        )
+                    ),
+                    JSON_PRETTY_PRINT
+                ));
+                return $response->withHeader("Content-Type", 'application/json')->withStatus(400);
+            }
+
+            $statement = $db->prepare('INSERT INTO comments (id, postId, email, name, body) VALUES (:id, :postId, :email, :name, :body)');
+
+            $statement->bindParam(':id', $id, PDO::PARAM_INT);
+            $statement->bindParam(':postId', $postId, PDO::PARAM_INT);
+            $statement->bindParam(':email', $email, PDO::PARAM_STR);
+            $statement->bindParam(':name', $name, PDO::PARAM_STR);
+            $statement->bindParam(':body', $body, PDO::PARAM_STR);
+            
+
+            $result = $statement->execute();
+
+            if (!$result) {
+                $response->getBody()->write(json_encode(
+                    array(
+                        "error" => array(
+                            'text' => "Ekleme sırasında hata oluştu"
+                        )
+                    ),
+                    JSON_PRETTY_PRINT
+                ));
+                return $response->withHeader("Content-Type", 'application/json')->withStatus(500);
+            }
+        }
+
+        $response->getBody()->write(json_encode(
+            array(
+                "success" => array(
+                    'text' => "Tüm yorumlar eklendi!"
+                )
+            ),
+            JSON_PRETTY_PRINT
+        ));
+        return $response->withHeader("Content-Type", 'application/json')->withStatus(200);
+    } catch (PDOException $e) {
+        $response->getBody()->write(json_encode(
+            array(
+                "error" => array(
+                    "text" => $e->getMessage(),
+                    "code" => $e->getCode()
+                )
+            ),
+            JSON_PRETTY_PRINT
+        ));
+        return $response->withHeader("Content-Type", 'application/json')->withStatus(500);
+    } finally {
+        $db = null;
+    }
+
+    return $response;
+});
+
+
 
 $app->run();
--- posts/index.php
+++ posts/index.php
@@ -1,4 +1,5 @@
 <?php
+
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
@@ -24,7 +25,6 @@ $app->get('/slim4/posts/', function (Request $request, Response $response, $args
                 ->withHeader("Content-Type", 'application/json')
                 ->withStatus(200);
         }
-
     } catch (PDOException $e) {
         $response->getBody()->write(json_encode(
             array(
@@ -36,6 +36,129 @@ $app->get('/slim4/posts/', function (Request $request, Response $response, $args
             JSON_PRETTY_PRINT
         ));
         return $response->withHeader("Content-Type", 'application/json')->withStatus(500);
+    } finally {
+        $db = null;
+    }
+    return $response;
+});
+
+
+// adding posts from given example link
+$app->post('/slim4/posts/add', function (Request $request, Response $response, $args) {
+    $db = new Db();
+    try {
+        $db = $db->connect();
+
+        $data = $request->getBody()->getContents();
+        $posts = json_decode($data, true);
+
+        if (!$posts) {
+            $response->getBody()->write(json_encode(
+                array(
+                    "error" => array(
+                        'text' => "Eksik veya hatalı veri"
+                    )
+                ),
+                JSON_PRETTY_PRINT
+            ));
+            return $response->withHeader("Content-Type", 'application/json')->withStatus(400);
+        }
+
+        foreach ($posts as $post) {
+            $userId = $post["userId"] ?? null;
+            $title = $post["title"] ?? null;
+            $body = $post["body"] ?? null;
+
+            if (!$userId || !$title || !$body) {
+                $response->getBody()->write(json_encode(
+                    array(
+                        "error" => array(
+                            'text' => "Eksik parametre"
+                        )
+                    ),
+                    JSON_PRETTY_PRINT
+                ));
+                return $response->withHeader("Content-Type", 'application/json')->withStatus(400);
+            }
+
+            $statement = $db->prepare('INSERT INTO posts (userId, title, body) VALUES (:userId, :title, :body)');
+
+            $statement->bindParam(':userId', $userId, PDO::PARAM_INT);
+            $statement->bindParam(':title', $title, PDO::PARAM_STR);
+            $statement->bindParam(':body', $body, PDO::PARAM_STR);
+
+            $result = $statement->execute();
+
+            if (!$result) {
+                $response->getBody()->write(json_encode(
+                    array(
+                        "error" => array(
+                            'text' => "Ekleme sırasında hata oluştu"
+                        )
+                    ),
+                    JSON_PRETTY_PRINT
+                ));
+                return $response->withHeader("Content-Type", 'application/json')->withStatus(500);
+            }
+        }
+
+        $response->getBody()->write(json_encode(
+            array(
+                "success" => array(
+                    'text' => "Tüm postlar eklendi!"
+                )
+            ),
+            JSON_PRETTY_PRINT
+        ));
+        return $response->withHeader("Content-Type", 'application/json')->withStatus(200);
+    } catch (PDOException $e) {
+        $response->getBody()->write(json_encode(
+            array(
+                "error" => array(
+                    "text" => $e->getMessage(),
+                    "code" => $e->getCode()
+                )
+            ),
+            JSON_PRETTY_PRINT
+        ));
+        return $response->withHeader("Content-Type", 'application/json')->withStatus(500);
+    } finally {
+        $db = null;
+    }
+
+    return $response;
+});
+
+
+//a single post and comments
+$app->get('/slim4/posts/{id}/comments', function (Request $request, Response $response, $args) {
+
+    $db = new Db();
+    $db = $db->connect();
+
+    $id = $args['id'];
+
+    try {
+        // Comments for the post given in the url
+        $comments = $db->query("SELECT * FROM comments WHERE postId = $id")->fetchAll(PDO::FETCH_OBJ);
+
+        $response->getBody()->write(json_encode($comments, JSON_PRETTY_PRINT));
+        return $response
+            ->withHeader("Content-Type", 'application/json')
+            ->withStatus(200);
+    } catch (PDOException $e) {
+        $response->getBody()->write(json_encode(
+            array(
+                "error" => array(
+                    "text" => $e->getMessage(),
+                    "code" => $e->getCode()
+                )
+            ),
+            JSON_PRETTY_PRINT
+        ));
+        return $response->withHeader("Content-Type", 'application/json')->withStatus(500);
+    } finally {
+        $db = null;
     }
 
     return $response;
